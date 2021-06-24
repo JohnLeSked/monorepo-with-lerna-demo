cd006d1b0d4fbd834bd66d6ce7ea70816e221860
JohnLeSked <nguyen.le@skedulo.com>
2021-06-24 22:32:28 +0700

    Update App.tsx

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. {add(2,2)}
+          Edit <code>src/App.tsx</code> and save to reload... {add(2,2)}
         </p>
         <a
           className="App-link"
